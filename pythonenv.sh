4c80ac17fbb9d27322f280ecef492f988d95eb8b
asogaard <andreas.sogaard@gmail.com>
2017-05-03 16:20:10 +0200

    Add python setup script for lxplus

---
+++ pythonenv.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Script for enabling several useful, scientific python packages on lxplus.
+
+# Fix setup warning messages
+export LC_ALL="en_US.UTF-8"
+export LANG="en_US.UTF-8"
+
+# Set up LCG 88 version of SWAN
+source /cvmfs/sft.cern.ch/lcg/views/LCG_88/x86_64-slc6-gcc49-opt/setup.sh
